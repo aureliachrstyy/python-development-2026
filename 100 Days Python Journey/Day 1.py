60247687dc4c29303f32fa87ecd32be98e51bd20
aureliachrstyy <bmybb59wwb@privaterelay.appleid.com>
2026-02-28 12:51:56 +0700

    Day 1

---
+++ 100 Days Python Journey/Day 1.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator")
+city=input("What's the name of your city you grew up in?")
+print(city)
+pet=input("What's the name of your pet?")
+print(pet)
+print(("Your band name could be:" + city + " " + pet))
